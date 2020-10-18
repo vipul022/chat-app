c737f8d13e5ab42e889638111a269ca22eb4bd7d
LachlynR <lachyreynolds@outlook.com>
2020-10-19 09:54:45 +1100

    Added some files

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const PORT = 5000;
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, "../client")));
+
+app.get("/", (req, res) => {
+  console.log("on root route");
+});
 
 app.listen(PORT, () => {
   console.log(`Listening at localhost:${PORT}`);
