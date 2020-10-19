5c3a5343231665dc2390299cdf77ddd96d1b7389
LachlynR <lachyreynolds@outlook.com>
2020-10-20 10:03:24 +1100

    cleaned up some files

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,57 +1,28 @@
-const UserModel = require("../models/user")
-const passport = require("passport")
+const UserModel = require("../models/user");
+const passport = require("passport");
 
-const registerNew = (req, res)=> {
-// console.log("registerNew=>", req.body);
-res.render("auth/register")
-
+const registerNew = (req, res) => {
+  // console.log("registerNew=>", req.body);
+  res.render("auth/register");
 };
-
-// const registerCreate = (req, res) => {
-// console.log("registerCreate=>", req.body)
-// const newUserHandler = (user) => {
-// //! req.login provided by passport
-// console.log("newUserHandler=>", user)
-// req.login(user, (err) => {
-//     if(err) {
-// console.log("if=>")
-//       next(err)
-//     }else{
-// console.log("else=>")
-//       res.redirect("/")
-//     }
-//     }) 
-// }
-// const {username, password} = req.body;
-
-// UserModel.create({username, password})
-// // .then((user) => newUserHandler(user))
-// .then((user) => console.log("user=>", user))
-
-// }
 
 const registerCreate = (req, res, next) => {
-console.log("registerCreate=>", req.body);
-const newUserHandler = (user) => {
-//! req.login provided by passport
-// console.log("newUserHandler=>", user);
-req.login(user, (err) => {
-if (err) {
-// console.log("if=>");
-next(err);
-} else {
-// console.log("else=>");
-res.redirect("/");
-}
-});
-};
-const { username, password } = req.body;
+  console.log("registerCreate=>", req.body);
+  const newUserHandler = (user) => {
+    //! req.login provided by passport
+    req.login(user, (err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  };
+  const { username, password } = req.body;
 
-UserModel.create({ username, password })
-.then((user) => newUserHandler(user))
-// .catch(next(err))
-// .then((user) => console.log("user=>", user));
+  UserModel.create({ username, password }).then((user) => newUserHandler(user));
+  // .catch(next(err))
+  // .then((user) => console.log("user=>", user));
 };
 
-
-module.exports = {registerNew, registerCreate}
+module.exports = { registerNew, registerCreate };
